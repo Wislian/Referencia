7b02ba862eb980644cfdf1c4c3e86106b64a4198
Sebastián Gómez Valencia <98537540+sgomez1205@users.noreply.github.com>
2022-03-30 10:28:20 -0500

    DailyEmployee
    Falta terminar

--- DailyEmployee.cpp
+++ DailyEmployee.cpp
@@ -1,1 +1,5 @@
+//
+// Created by sebas on 29/03/2022.
+//
+
 #include "DailyEmployee.h"
--- DailyEmployee.h
+++ DailyEmployee.h
@@ -1,9 +1,28 @@
-#ifndef DAILYEMPLOYEE_H
-#define DAILYEMPLOYEE_H
+//
+// Created by sebas on 29/03/2022.
+//
 
-class DailyEmployee {
+#ifndef EJERCICIO_CLASE_DAILYEMPLOYEE_H
+#define EJERCICIO_CLASE_DAILYEMPLOYEE_H
 
+#include <string.h>
+#include "Employee.h"
+
+
+class DailyEmployee : public Employee
+        {
+public:
+    DailyEmployee(const string &, const string &, const string &, int workedDays, double dailyWage );
+    virtual double earnings() const;
+    virtual void print() const;
+
+private:
+    string firstName;
+    string lastName;
+    string socialSecurityNumber;
+    int workedDays;
+    double dailyWage;
 };
 
 
-#endif
+#endif //EJERCICIO_CLASE_DAILYEMPLOYEE_H
